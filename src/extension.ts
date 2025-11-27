75fb303524c3223e85fd72849e33b06aac1148eb
0xVector <hello@0xvector.me>
2025-11-27 13:03:07 +0100

    Add activation command

--- src/extension.ts
+++ src/extension.ts
@@ -8,15 +8,15 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "msim-debugger" is now active!');
+	console.log('msim-debugger activated!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('msim-debugger.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('msim-debugger.activate', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from MSIM debugger!');
+		vscode.window.showInformationMessage('Activated MSIM debugger!');
 	});
 
 	context.subscriptions.push(disposable);
